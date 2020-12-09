2b35e56b10bfe3a1a07ee7fab919ce0f11319fac
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2020-12-09 07:40:40 +0100

    AppBar color

--- Canvas_Flutter/canvas_flutter/lib/main.dart
+++ Canvas_Flutter/canvas_flutter/lib/main.dart
@@ -52,6 +52,7 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
+        backgroundColor: Color(0xFF6200EE),
         title: Text(this.title,
           style: TextStyle(
             color: Colors.black,
